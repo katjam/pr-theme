8d5ccef58b78a984fa7077c358321cf842d79094
katjam <katja@neontribe.co.uk>
2018-03-07 18:39:02 +0000

    Footer and justify text

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,8 @@
+<div class="container before-footer">
+  <div class="row col-md-12">
+    <span class="rics">Regulated by RICS</span>
+  </div>
+</div>
 <footer id="footer" class="content-info">
   <div class="container">
     <div class="row">
