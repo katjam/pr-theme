0f3a9a726b54c2f2d6918a363404c78313586ea6
katjam <katja@neontribe.co.uk>
2018-04-29 10:58:36 +0100

    Update pdf field layout

--- templates/content-page.php
+++ templates/content-page.php
@@ -8,17 +8,15 @@
   <?php $pr_img_text_sets = get_post_meta($post->ID, 'pr_img_text_sets', true);
         if ( $pr_img_text_sets ) : ?>
           <?php foreach ( $pr_img_text_sets as $field ) { ?>
-            <div class="d-md-flex pb-md-2">
-              <div class="pr-md-2">
-                <?php if($field['image'] != '') echo '<img src="'. esc_attr( $field['image'] ) . '" style="max-width: 350px;" />'; ?>
-              </div>
+          <div class="d-md-flex pb-md-2">
+              <?php if($field['image'] != '') echo '<div class="pr-md-2"><img src="'. esc_attr( $field['image'] ) . '" style="max-width: 350px;" /></div>'; ?>
               <div>
                 <?php if($field['heading'] != '') echo '<h3>'. esc_attr( $field['heading'] ) . '</h3>'; ?>
                 <?php if($field['text'] != '') echo '<p>'. esc_attr( $field['text'] ) . '</p>'; ?>
               </div>
-              <div>
-                <?php if($field['pdf_src'] && $field['pdf_src']['url'] != '') echo '<a href="'.$field['pdf_src']['url'].'" role="button" class="button">Download Pdf</a>'; ?>
-              </div>
+            </div>
+            <div>
+              <?php if($field['pdf_src'] != '') echo '<a href="'.$field['pdf_src'].'" role="button" class="button"><span class="fa fa-file-pdf-o"></span>Download Pdf</a>'; ?>
             </div>
           <?php } ?>
         <?php endif; ?>
